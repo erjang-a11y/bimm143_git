9abb8009080538f2c6d472511e25bd0a9ddf7da9
Erin Jang <erjang@ucsd.edu>
2026-05-19 17:46:32 -0700

    adding classes 4-7

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Erin Jang (PID: A17713992)"
+#' date: "April 9, 2026"
+#' ---
+
+# This is just a text file
+x <- 1:50
+plot(x, sin(x), type="l", col="blue", lwd=3)
